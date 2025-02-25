3878b2af9cb063e65b0f98e0dd786256c589327b
bozhidarzlatev <b.zlatev@outlook.com>
2025-02-25 19:15:23 +0200

    css fixes

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -7,7 +7,7 @@ export default function  Calculator() {
     const [history , setHistory ] = useState('No input')
     const [query, setQuery] = useState('')
     const [total, setTotal] = useState(0)
-    
+    const [keyPressed, setKeyPressed] = useState(null)    
 
     function ButtonClicked(value) {
         if (total === 'Error' || query === 'AC') {
@@ -15,11 +15,25 @@ export default function  Calculator() {
             setTotal(0)
         }
 
+        console.log(value);
+        
         setQuery(prev => prev + value)       
         
         if (value === 'AC') {
             setQuery('AC');
             setTotal(0)
+            return
+        }
+
+        if (value === '√') {
+            try {
+                setTotal(Math.sqrt(parseFloat(query)));
+                setHistory(`√(${query})`)
+                setQuery('')
+            } catch (error) {
+                setTotal('Error')
+                setQuery('Error')
+            }
             return
         }
 
@@ -51,28 +65,38 @@ export default function  Calculator() {
     useEffect(() => {
         function handleKeyPress(event) {
             const {key} = event
-            console.log(key);
 
             if (keys.includes(key)) {
+                setKeyPressed(key)
                 ButtonClicked(key)
             } else if ( key === 'Enter') {
+                setKeyPressed('=')
                 ButtonClicked('=')
                 
             } else if ( key === 'Backspace') {
+                setKeyPressed('<')
                 ButtonClicked('<')
                 
             }
             
         }
+        
+        function handleKeyUp(params) {
+            setKeyPressed(null)
+            
+        }
+
         window.addEventListener('keydown', handleKeyPress)
+        window.addEventListener('keyup', handleKeyUp)
         
         return () => {
             window.removeEventListener('keydown', handleKeyPress)
+            window.removeEventListener('keyup', handleKeyUp)
 
         };
     }, [query])
 
-    const keys = ["AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "<", "0", ",", "="];
+    const keys = ["AC", "√", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "<", "0", ",", "="];
     const orangeKeys = ["/", "*", "-", "+", "="];
 
     return (
@@ -80,14 +104,15 @@ export default function  Calculator() {
           <h3>Calculator</h3>
             <div className="calculator">
                 <div className='display'>
+                    <div className='total'>Total: {total}</div>
                     <div className='query'>{query !== '' ? query: history}</div>
-                    <div className='string'>Total: {total}</div>
                 </div>
                 <div className="keypad">
                     {keys.map((key) => (
                         <div 
                             key={key} 
-                            className={`key ${orangeKeys.includes(key) ? 'orange' : ''}`} 
+                            className={`key ${orangeKeys.includes(key) ? 'orange' : ''}
+                        ${keyPressed === key ? 'pressed' : ""}`} 
                             onClick={() => ButtonClicked(key)}
                         >
                             {key}
